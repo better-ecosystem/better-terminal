df31eb822c18c3155335ad6566c3f41b277d127b
FelipeFMA <felipefmavelar@gmail.com>
2025-05-30 14:30:35 -0300

    Add title bar toggle functionality and context menu to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use gtk4::prelude::*;
 use vte4::prelude::*;
 
-use gtk4::{gio, Application, Box, Orientation};
+use gtk4::{gio, Application, Box, Orientation, PopoverMenu, GestureClick};
 use libadwaita::{ApplicationWindow, HeaderBar};
 use vte4::Terminal;
 
@@ -38,8 +38,8 @@ fn build_ui(app: &Application) {
             }
         },
     );
-
     let header_bar = HeaderBar::new();
+    header_bar.set_show_end_title_buttons(true); // Ensure default title buttons are shown
 
     let content_box = Box::new(Orientation::Vertical, 0);
     content_box.append(&header_bar);
@@ -52,6 +52,40 @@ fn build_ui(app: &Application) {
         .default_height(400)
         .content(&content_box)
         .build();
+
+    let menu = gio::Menu::new();
+    menu.append(Some("Show Title Bar"), Some("win.toggle_title_bar"));
+
+    let popover = PopoverMenu::from_model(Some(&menu));
+    popover.set_parent(&terminal);
+
+    let gesture = GestureClick::new();
+    gesture.set_button(3);
+    let popover_clone = popover.clone();
+    gesture.connect_pressed(move |_, _, x, y| {
+        let rect = gtk4::gdk::Rectangle::new(x as i32, y as i32, 1, 1);
+        popover_clone.set_pointing_to(Some(&rect));
+        popover_clone.popup();
+    });
+    terminal.add_controller(gesture);
+
+    let header_bar_clone = header_bar.clone();
+    let toggle_title_bar_action = gio::SimpleAction::new_stateful(
+        "toggle_title_bar",
+        None,
+        &glib::Variant::from(true),
+    );
+    toggle_title_bar_action.connect_activate(move |action, _| {
+        let current_state = action
+            .state()
+            .expect("Could not get state.")
+            .get::<bool>()
+            .expect("Could not get bool state.");
+        let new_state = !current_state;
+        header_bar_clone.set_visible(new_state);
+        action.set_state(&glib::Variant::from(new_state));
+    });
+    window.add_action(&toggle_title_bar_action);
 
     let window_clone = window.clone();
     terminal.connect_child_exited(move |_terminal, _status| {
